3644e1f9b19fdf9444dac49e39b59925dec4b529
Ngoc Don <kaitoukid2701@gmail.com>
2015-12-07 01:26:53 +0700

    Add Hosting info

--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
+            'database'  => env('DB_DATABASE', 'hoasinhvie_laptopcali'),
+            'username'  => env('DB_USERNAME', 'hoasinhvie_don'),
+            'password'  => env('DB_PASSWORD', 'Don20130941'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
