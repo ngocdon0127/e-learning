85e76351e0eb963d180e3327a9d0bc29dbcade34
Hola Nguyen <kevinhoa95@gmail.com>
2015-12-07 02:40:13 +0700

    Add UI

--- resources/views/admin/addpost.blade.php
+++ resources/views/admin/addpost.blade.php
@@ -1,23 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Form trong Laravel 5</title>
-</head>
-<body>
-    <h1>Them Bai Viet Moi</h1>
-    {!! Form::open(['url' => '/admin/addpost']) !!}
-        {!! Form::select('CourseID', \App\Courses::getColumn('Title')) !!}
-{{--        {!! Form::select('CourseTitle', array(1=>1, 3=>4)) !!};--}}
-        {!! Form::label('FormatID', 'FormatID: ') !!}
-        {!! Form::text('FormatID') !!}
-        {!! Form::label('Question', 'Question: ') !!}
-        {!! Form::text('Question') !!}
-        {!! Form::label('Photo', 'Photo: ') !!}
-        {!! Form::text('Photo') !!}
-        {!! Form::label('Description', 'Description: ') !!}
-        {!! Form::text('Description') !!}
-        {!! Form::submit('Thêm') !!}
-    {!! Form::close() !!}
-</body>
-</html>
+@extends('layouts.master')
+@section('head.title')
+    Them bai viet moi
+@endsection
+@section('body.content')
+<<div class="container">
+    <<div class="col-sm-6 col-sm-offset-3">
+        <h1>Thêm bài viết mới</h1>
+        {!! Form::open(['url' => '/admin/addpost']) !!}
+            <div class="form-group">
+            {!! Form::label('FormatID', 'FormatID: ') !!}
+            {!! Form::text('FormatID') !!}
+            {!! Form::select('CourseID', \App\Courses::getColumn('Title')) !!}
+             {{--  {!! Form::select('CourseTitle', array(1=>1, 3=>4)) !!};--}}
+            </div>
+            <div class="form-group">
+            {!! Form::label('Question', 'Question: ') !!}
+            {!! Form::text('Question') !!}
+            </div>
+            <div class="form-group">
+            {!! Form::label('Photo', 'Photo: ') !!}
+            {!! Form::text('Photo') !!}
+            </div>
+            <div class="form-group">
+            {!! Form::label('Description', 'Description: ') !!}
+            {!! Form::text('Description') !!}
+            </div>
+            {!! Form::submit('Thêm', ['class' => 'btn btn-primary']) !!}
+        {!! Form::close() !!}
+    </div>
+</div>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,9 @@
+<div class="jumbotron">
+  <div class="container">
+    <h1 style="color: #00fa9a">CÂU LẠC BỘ TEC</h1>
+    <p><i>Thỏa sức đam mê, thỏa sức sáng tạo :)) :))</i></p>
+    <p>
+      <a class="btn btn-primary btn-lg">Learn more</a>
+    </p>
+  </div>
+</div>
