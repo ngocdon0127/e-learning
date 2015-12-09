0ec711662366c36b82cca289eeab52fb1dc78405
Ngoc Don <kaitoukid2701@gmail.com>
2015-12-10 02:28:07 +0700

    Fix Landing Page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,32 +1,20 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: NgocDon
- * Date: 12/7/2015
- * Time: 1:09 AM
- */?>
-
-        <!DOCTYPE html>
-<html>
-<head>
-    <title>Laravel</title>
-
-    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-    <style>
-
-    </style>
-</head>
-<body>
-<div class="container">
-    <div class="content">
+@extends('layouts.main')
+@section('head.title')
+    ADD POST
+@endsection
+@section('body.content')
+    <div class="container">
         <ul>
-            @foreach ($allcourse as $course)
-                <a href="/course/{{$course['id']}}">{{$course['Title']}}</a>
-            @endforeach
-            <a href="/admin/addcourse">Add Course</a>
+        @foreach (\App\Courses::all()->toArray() as $course)
+            <li><a href="/course/{{$course['id']}}">{{$course['Title']}}</a></li>
+        @endforeach
         </ul>
+        <a href="/admin/addcourse">Add Course</a>
     </div>
-</div>
-</body>
-</html>
+
+@endsection
+
+
+<ul>
+
+</ul>
--- resources/views/viewcourse.blade.php
+++ resources/views/viewcourse.blade.php
@@ -1,25 +1,9 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: NgocDon
- * Date: 12/6/2015
- * Time: 7:41 PM
- */?>
-
-        <!DOCTYPE html>
-<html>
-<head>
-    <title>Laravel</title>
-
-    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-    <style>
-
-    </style>
-</head>
-<body>
-<div class="container">
-    <div class="content">
+@extends('layouts.main')
+@section('head.title')
+    ADD POST
+@endsection
+@section('body.content')
+    <div class="container">
         <div class="title">{{$Title}}</div>
         <ul>
             @foreach ($posts as $key => $value)
@@ -28,7 +12,6 @@
         </ul>
         <a href="/admin/addpost">Thêm câu hỏi</a>
     </div>
-</div>
-</body>
-</html>
+
+@endsection
 
