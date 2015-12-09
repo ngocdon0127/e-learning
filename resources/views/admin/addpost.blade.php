19a10c1a04855f6d6593aa9fe0eb8e8805583327
Ngoc Don <kaitoukid2701@gmail.com>
2015-12-10 04:17:50 +0700

    Add 'Check Uploading File Format' Feature

--- resources/views/admin/addpost.blade.php
+++ resources/views/admin/addpost.blade.php
@@ -5,7 +5,7 @@ ADD POST
 @section('body.content')
 <div class="container">
 
-        {!! Form::open(['url' => '/admin/addpost', 'class'=>'form-horizontal', 'files' => true]) !!}
+        {!! Form::open(['name' => 'addPostForm', 'url' => '/admin/addpost', 'class'=>'form-horizontal', 'files' => true]) !!}
             <div class="col-sm-offset-3">
                 <h1>Thêm bài viết mới</h1>
             </div>
@@ -31,7 +31,7 @@ ADD POST
              <div class="form-group">
                 {!! Form::label('Photo', 'Photo : ',['class' => 'col-md-3 control-label']) !!}
                 <div class="col-sm-6">
-                    {!! Form::file('Photo','',['class'=>'form-control']) !!}
+                    {!! Form::file('Photo', ['accept' => 'image/jpeg, image/png, image/gif']) !!}
                 </div>
             </div>
             <div class="form-group">
@@ -40,8 +40,44 @@ ADD POST
                     {!! Form::text('Description','',['class'=>'form-control']) !!}
                 </div>
             </div>
+            <div class="form-group">
+                <div class="col-sm-6">
+                    {!! Form::label('', '',['class' => 'col-md-3 control-label']) !!}
+                    {!! Form::label('Error', '',['id' => 'error', 'class' => 'col-md-3 control-label', 'style' => 'display: none;']) !!}
+                </div>
+            </div>
             <div class="col-sm-offset-3 col-sm-10">
-                {!! Form::submit('Thêm',['class' => 'btn btn-default']) !!}
+                <script type="text/javascript">
+                    function ob(x){
+                        return document.getElementById(x);
+                    }
+                    function submitForm(){
+                        var acceptedType = ['image/jpeg', 'image/png', 'image/gif'];
+//                        console.log('clicked');
+                        var photo = ob('Photo');
+                        var type = ob('error').innerHTML = photo.files[0].type;
+                        var check = false;
+                        for(i = 0; i < acceptedType.length; i++){
+                            if (type == acceptedType[i]){
+                                check = true;
+                                break;
+                            }
+                        }
+                        if (!check){
+//                            console.log('not ok');
+                            ob('error').style.display = 'block';
+                            ob('error').innerHTML = 'Chỉ chọn file ảnh.';
+                        }
+                        else{
+//                            console.log('ok');
+                            ob('error').style.display = 'none';
+                            document.addPostForm.submit();
+                        }
+//                        ob('error').innerHTML = photo.value;
+
+                    }
+                </script>
+                {!! Form::button('Thêm',['class' => 'btn btn-default', 'onClick' => 'submitForm()']) !!}
             </div>
         {!! Form::close() !!}
 </div>
