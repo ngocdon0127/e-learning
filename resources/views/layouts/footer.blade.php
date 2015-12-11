2bb64426d44effffa3182b96d79efdbe5b02288d
Hola Nguyen <kevinhoa95@gmail.com>
2015-12-12 03:45:20 +0700

    add a template for other pages

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
 
-	<div class="footer-container">
-			<p>Copyright &copy; 3 Chàng Code</p>
-	</div>
+<footer class="navbar navbar-default navbar-fixed-bottom" role="navigation">
+	   <center style = "padding-top: 10px;"><b>Copyright &copy; by Team Thái Bình</b></center>
+</footer>
