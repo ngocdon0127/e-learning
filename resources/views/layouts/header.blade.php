74e46931a6d651afcdcce12e899ae3ba0d27c760
Ngoc Don <kaitoukid2701@gmail.com>
2015-12-12 04:43:02 +0700

    Fix Header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,2 +1,3 @@
   <div class="col-sm-offset-3 col-sm-6 col-xs-offset-3 col-xs-6">
-    <header class="header">Learning English Together</header>
+    <header class="header"><a href="/">Learning English Together</a><br /><a href="/auth/login">Login</a> </header>
+  </div>
