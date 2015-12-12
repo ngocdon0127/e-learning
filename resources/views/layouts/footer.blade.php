1f01c57964bc630e12838a6a3e3a92897db51f24
Hola Nguyen <kevinhoa95@gmail.com>
2015-12-12 22:17:14 +0700

    add footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,3 @@
-<div class="footer container-fluid">
-	<div class="col-md-6 col-md-offset-3">
-			<p>Copyright &copy; 3 Chàng Code</p>
-	</div>
-</div>
+<footer class="navbar navbar-default navbar-fixed-bottom" role="navigation">
+	   <center style = "padding-top: 10px;"><b>Copyright &copy; by Team Thái Bình</b></center>
+</footer>
