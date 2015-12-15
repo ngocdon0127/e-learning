645104b75c124cfa3c68f964cc85ad0c84aa84a7
Hola Nguyen <kevinhoa95@gmail.com>
2015-12-15 18:55:54 +0700

    merge

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-default col-sm-12" data-spy="affix" data-offset-top="197" role="navigation" style = "color: #EAEAEA; class="active"">
+<nav class="navbar navbar-default" role="navigation" style = "color: #EAEAEA;">
 	<!-- Brand and toggle get grouped for better mobile display -->
 	<!-- <a id="HomeSmall" style="text-decoration: none;" href="">TEC Club</a> -->
 	<div class="navbar-header">
@@ -13,7 +13,7 @@
 
 	<!-- Collect the nav links, forms, and other content for toggling -->
 	<!-- <div class="col-sm-offset-3"> -->
-	<div class="collapse col-sm-offset-2 navbar-collapse navbar-ex1-collapse">
+	<div class="collapse col-sm-offset-3 navbar-collapse navbar-ex1-collapse">
 		<ul class="nav navbar-nav">
 			<li class="active"><a href="/">Home</a></li>
 			<li class="active"><a href="/admin">Admin</a></li>
@@ -21,7 +21,7 @@
 			<li class="active"><a href="#">Quizzes</a></li>
 			<li class="active"><a href="#">Video</a></li>
 			<li class="dropdown active">
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown">More<b class="caret"></b></a>
+				<a id="dropDown" href="#" class="dropdown-toggle" data-toggle="dropdown">More<b class="caret"></b></a>
 				<ul class="dropdown-menu">
 					<li><a href="#">Grammar</a></li>
 					<li><a href="#">Another action</a></li>
@@ -29,19 +29,16 @@
 					<li><a href="#">Separated link</a></li>
 				</ul>
 			</li>
-			<li class="active">
-				<link src="//www.facebook.com/plugins/follow?href=https%3A%2F%2Fwww.facebook.com%2Fkevinhoa95&amp;" scrolling="no" frameborder="0" style="border:none; overflow:hidden;"></link>
-			</li>
 		</ul>
 		<form class="navbar-form navbar-right" role="search">
-			<div class="form-group">
-				<input type="text" class="form-control" placeholder="Search">
+		<!-- 	<div class="form-group">
+					<input type="text" class="form-control" placeholder="Search">
 
-			<!-- <button type="submit" class="btn btn-default">Submit</button> -->
+					<button type="submit" class="btn btn-default">Submit</button>
 			        <button type="button" class="btn btn-default btn-sm">
           				<span class="glyphicon glyphicon-search"></span> Submit 
         			</button>
-						</div>
+				</div> -->
 			@if (auth()->user())
 			<!-- <button type="button" class="btn btn-default"><a href="/auth/logout">Logout</a></button> -->
 			<a class="btn btn-primary" href="/auth/logout" role="button">Logout</a>
