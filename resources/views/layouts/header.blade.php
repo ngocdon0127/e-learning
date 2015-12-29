621b938fb9e917eed8d342e76f121753ca5e3cdc
Hola Nguyen <kevinhoa95@gmail.com>
2015-12-30 00:33:16 +0700

    merger

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,12 +1,12 @@
 <header>
-  <div class="" style="margin-bottom: 0px;">
-  	<div class="container">
-  		<h1 class="header">Evangels English</h1>
-  		<p class="header">Know English. Know the World</p>
-  		<p class="header">
-  			<a class="btn btn-primary btn-lg">Learn more</a>
-  		</p>
-  	</div>
+  <div class="jumbotron" style="margin-bottom: 0px;">
+    <div class="container">
+      <h1 class="header">Evangels English</h1>
+      <p class="header">Know English. Know the World</p>
+      <p class="header">
+        <a class="btn btn-primary btn-lg">Learn more</a>
+      </p>
+    </div>
   </div>
 </header>
 
