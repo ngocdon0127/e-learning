4d805830cfcc6cc3393083d09968bb4f22049073
duckien95 <keanronal@gmail.com>
2016-01-11 00:16:19 +0700

    fix 'Back to top' and header

--- resources/views/viewpost.blade.php
+++ resources/views/viewpost.blade.php
@@ -163,7 +163,9 @@
     <div class="form-control" id="resultText" style="display: none; height: 200px;">
         <b class="title" id="writeResult"></b> <br />
     </div>
-	<a class="btn btn-primary" href="#">Back to top</a>
+	<div>
+        <a class="btn btn-primary" href="#">Back to top</a>
+    </div>
     <div class="fb-comments" data-href="{!! 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']!!}" data-width="500" data-numposts="5"></div>
     <div class="fb-like" data-href="{!! 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']!!}" data-width="450" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
 @endsection
