bd3b96d8ac6491c3d974bed0b0bf733136fc65d1
Hola Nguyen <kevinhoa95@gmail.com>
2016-01-15 01:37:51 +0700

    cleary route, fix destroy post, course, question alert

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,10 +6,10 @@
 	<h1 class="title">Các khóa học</h1>
 	<ul class="list-group">
 	@foreach (\App\Courses::all()->toArray() as $course)
-	    <li class="list-group-item list-group-item-warning"><a href="/admin/course/{{$course['id']}}">{{$course['Title']}}</a></li>
+	    <li class="list-group-item list-group-item-warning"><a href="{{route('admin.viewcourse',$course['id'])}}">{{$course['Title']}}</a></li>
 	@endforeach
 	</ul>
  	@if ((auth()->user()) && (auth()->user()->admin == 1))
-	<a href="/admin/addcourse" class="btn btn-primary">Add Course</a>
+	<a href="{{route('admin.addcourse')}}" class="btn btn-primary">Add Course</a>
 	 @endif
 @endsection
