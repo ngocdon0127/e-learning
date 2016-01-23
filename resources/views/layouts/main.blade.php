54ba1727e8b75666a1d5baf44eff786900225a32
Ngoc Don <kaitoukid2701@gmail.com>
2016-01-24 00:01:35 +0700

    Use Zopim chat

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -87,6 +87,15 @@
 			}); //end of ajax
 		}
 	</script>
+	<!--Start of Zopim Live Chat Script-->
+	<script type="text/javascript">
+	window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+	d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+	_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+	$.src="//v2.zopim.com/?3dgbwbmhc2aL8G86QOWgVfX26ycur6x0";z.t=+new Date;$.
+	type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+	</script>
+	<!--End of Zopim Live Chat Script-->
 	@yield('head.css')
 </head>
 @if (auth() && (auth()->user()))
