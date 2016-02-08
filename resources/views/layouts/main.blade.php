fc8159b54b524a7cc72e9f09202df52e2e9f9716
Ngoc Don <kaitoukid2701@gmail.com>
2016-02-08 23:23:54 +0700

    Integrate Google Analytics for site

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -118,6 +118,17 @@
 @else
 <body onload="saveIP();">
 @endif
+	<!-- Start of Google Analytics -->
+	<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-73481404-1', 'auto');
+	ga('send', 'pageview');
+	</script>
+	<!-- End of Google Analytics -->
 	<div class="wrapper side">
 		@include('layouts.header')
 		@include('layouts.navbar')
